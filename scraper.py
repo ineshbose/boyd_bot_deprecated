84033cc77483c3203904b3843e9a5b2c99fa471e
ineshbose <ineshbose@yahoo.com>
2020-02-15 23:02:14 +0000

    sjfsjdfsodjf

--- scraper.py
+++ scraper.py
@@ -9,6 +9,7 @@ options = webdriver.ChromeOptions()
 options.add_argument('--headless')
 options.add_argument('--disable-gpu')
 options.add_argument('--no-sandbox')
+options.add_argument('--disable-dev-shm-usage')
 options.binary_location = GOOGLE_CHROME_PATH
 browser = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH, chrome_options=options)
 
