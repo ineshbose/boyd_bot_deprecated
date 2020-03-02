93f2b2dc674148f05ae31221dd5695cf3d202c30
ineshbose <ineshbose@yahoo.com>
2020-03-02 20:04:49 +0000

    Login fix

--- app.py
+++ app.py
@@ -26,8 +26,8 @@ bot = Bot(PAGE_ACCESS_TOKEN)
 @app.route('/', methods=['GET'])
 def verify():
     if request.args.get("hub.mode") == "subscribe" and request.args.get("hub.challenge"):
-        if not request.args.get("hub.verify_token") == "gutbot":
-        #if not request.args.get("hub.verify_token") == os.environ.get("VERIFY_TOKEN"):
+        #if not request.args.get("hub.verify_token") == "gutbot":
+        if not request.args.get("hub.verify_token") == os.environ.get("VERIFY_TOKEN"):
             return "Verification token mismatch", 403
         return request.args["hub.challenge"], 200
     return "Hello World", 200
